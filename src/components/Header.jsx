ac06acca9998be4ae4d26e474db114a6f1079054
Matthew Caravaggio <matthew.caravaggio@gmail.com>
2019-12-10 10:37:30 -0500

    Header made

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,37 @@
 import React, {Component} from 'react'
+
+export default class Header extends Component {
+
+    render(){
+        return(
+            <header>
+                <div className='logo-container'>
+                    <img src='' />
+                </div>
+                <nav>
+                <div className='nav-button'>
+                        <h3>
+                            About
+                        </h3>
+                    </div>
+                    <div className='nav-button'>
+                        <h3>
+                            Skills
+                        </h3>
+                    </div>
+                    <div className='nav-button'>
+                        <h3>
+                            Work
+                        </h3>
+                    </div>
+                    <div className='nav-button'>
+                        <img src='' href=''/>
+                    </div>
+                    <div className='nav-button'>
+                        <img src='' href=''/>
+                    </div>
+                </nav>
+            </header>
+        )
+    }
+}
