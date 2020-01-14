e013f1f734a67bf9ff458811bbfafc7b8fc1e517
Matthew Caravaggio <matthew.caravaggio@gmail.com>
2020-01-14 17:47:24 -0500

    Adding gallery images

---
+++ src/components/Project-Gallery.js
@@ -0,0 +1,23 @@
+import blackjack_image from './images/project-images/Screenshot from 2019-12-11 09-20-53.png'
+import world_builder from './images/project-images/Screenshot from 2019-12-11 09-20-53.png'
+
+const devGallery = [
+    {
+        title: "Blackjack",
+        image: blackjack_image,
+        description: "Placeholder Text"
+    },
+    {
+        title: "World Builder",
+        image: world_builder,
+        description: "Placeholder Text"
+    }
+]
+
+const desGallery = [
+    {
+        title: "Blackjack",
+        image: blackjack_image,
+        description: "Placeholder Text"   
+    }
+]
