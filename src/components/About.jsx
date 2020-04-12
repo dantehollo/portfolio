5efdd75c72916e3211e9f0280c6df4750fc34e2e
Matthew Caravagio <matthew.caravaggio@protonmail.com>
2020-04-11 22:07:21 -0400

    Creating About structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,32 @@
 import React, {Component} from 'react'
-import portrait from './images/portraits/me.jpg'
+// import portrait from './images/portraits/me.jpg'
 
 export default class About extends Component {
     render() {
         return(
             <div class="about">
-                {/* <img src={portrait} className='portrait'/> */}
-                <p>
-                    Aliqua enim mollit commodo ipsum proident consequat voluptate proident. Laboris mollit laborum proident et aliqua sint non proident dolore officia nulla ut laboris. Culpa exercitation aliquip et id quis duis enim elit magna officia sint. Aute incididunt ipsum voluptate dolor minim occaecat sit culpa ullamco voluptate. Mollit enim enim irure sunt esse.
-                </p>
+                <div className="about-text-box">
+                    DIV HERE
+                    <h1 className="about-heading">
+                        Hey there!
+                    </h1>
+                    <p className="about-paragraph">
+                        This is me, Matthew Caravaggio, ex-designer turned Web Developer,
+                        currently in the process of reinventing myself and updating my skill set
+                        in the pursuit of making beautiful things.
+                        Only now, I make them with code.
+                    </p>    
+                    <p className="about-paragraph">
+                        I graduated from General Assembly's Atlana campus in December of 2019
+                        before the world blew up from their Software Engineering Immersive Program.
+                        My cohort mates and I spent three months working 50 hour weeks living and breathing MERN stack code.
+                        The end product were four projects, none of which I liked, so I made new ones.
+                        But enough about me...
+                    </p>
+                    <h1 className="about-heading">
+                        <span>You're just here to see what I can do, aren't you?</span>
+                    </h1>
+                </div>
             </div>
         )
     }
