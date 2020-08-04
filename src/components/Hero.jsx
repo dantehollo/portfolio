cef60218b011cd26d48660d785425d35437646ca
Matthew Caravagio <matthew.caravaggio@protonmail.com>
2020-08-04 18:29:25 -0400

    Still toying witht he css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,21 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Hero extends Component {
   render(){
     return(
-      <section className="full-size">
-        <div className="sphere"/>
-        <div className="hero-text-box">
-          <h1 className="hero-heading">
-            Welcome
-          </h1>
-          <h2 className="hero-subhead">
-            Designer turned Web Developer, reinventing and updating my skills
-            in the pursuit of
-            making beautiful things.
-          </h2>
-          <h2 className="last-line">
-            Only now, I make them with code.
-          </h2>
-        </div>
+      <section className="hero">
+        {/* <div className="sphere"> */}
+          <div className="hero-text-box">
+            <h1 id="hero-heading">
+              WELCOME
+            </h1>
+            <h2 id="hero-subhead">
+              I am a designer turned Web Developer, reinventing my skills
+              in the pursuit of
+              making beautiful things.
+            </h2>
+            <h2 id="last-line">
+              Only now, I make them with code.
+            </h2>
+          </div>
+        {/* </div> */}
       </section>
     )
   }
